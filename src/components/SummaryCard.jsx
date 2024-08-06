b60fa0658027f5234b212d7c177e1cfda8d1cf45
codeSparrow09 <anildudi092@gmail.com>
2024-08-06 20:17:26 +0530

    check

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 const SummaryCard = ({ title, value, icon, growth }) => {
     return (
-        <div className="bg-gray-800 p-4 rounded-lg flex-col items-center justify-between">
-            <div className="text-4xl">{icon}</div>
+        <div className="bg-gray-800 p-4 rounded-lg flex items-center justify-between">
             <div>
                 <h3 className="">{title}</h3>
-                <p className="text-white text-2xl">{value}</p>
+                <p className="text-white text-2xl font-bold">{value}</p>
                 <span className={`text-sm ${growth >= 0 ? 'text-green-400' : 'text-red-400'}`}>
                     {growth >= 0 ? `▲ ${growth}%` : `▼ ${growth}%`}
                 </span>
             </div>
+            <div className="text-4xl">{icon}</div>
+
         </div>
     );
 };
